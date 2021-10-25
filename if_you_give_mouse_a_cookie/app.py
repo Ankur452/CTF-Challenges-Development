3f9883905db1b58210c042dc441f1f2ccb33d0f1
Ankur Vats <Ankur452@users.noreply.github.com>
2021-10-25 10:46:54 +0530

    Add files via upload

--- if_you_give_mouse_a_cookie/app.py
+++ if_you_give_mouse_a_cookie/app.py
@@ -13,19 +13,16 @@ def welcome():
     resp.set_cookie('uid','ajdhewiuy98hij43bjy983hfyfew79gsdkn34y84g8gb483ut32g3bjy983hfyfew79gsdlkn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th4kjfoiwf98732dshf82r9whef98u2fih28fo29ub349th4lkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th438h3', max_age=60*60*24*365*2)
     resp.set_cookie('Nautilas_CSRF_Token','8tgh73gh74g4npurh8gh3t8ureg9gverg98hjrkn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th44hg98hkn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th43g9843g98h34g9839gh9436ghiuhfdkguq3209tukerjgh948ghoegh9opo4u3gih43g93', max_age=60*60*24*365*2)
     
-    resp.set_cookie('is_cookie_provided','True', max_age=60*60*24*365*2, secure='True', httponly='True')
+    resp.set_cookie('is_cookie_provided','True', max_age=60*60*24*365*2, secure='True')
     
     resp.set_cookie('utag-v1','hhhg1jg312oiberhwoikn34y84g8gb483ut32g3bjy983hfyfevw79gsdlkkn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th4jfoiwf98732dshf82r9whef98u2fih28fo29ub349th4eaovien3958g3j12f3j1g4kj25gj235khgh43tkjbvejhg8743t782tiurgkjsfhg98iuwh346t32hj412k', max_age=60*60*24*365*2)
     resp.set_cookie('uid-vi','ajdhewiuy98hij4vlkn34y84g8gb483utkn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th432g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th438h3', max_age=60*60*24*365*2)
     resp.set_cookie('Apigee_CSRF_Token','8tgh73g3g4554h54hj5356jhrt5h45356hh74g4npurh8gh3t8ureg9gvergkn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th498hjr4hg98h3g9843g98h34g9839gh9436ghiuhfdkguq3209tukerjgh948ghoegh9opo4u3gih43g93', max_age=60*60*24*365*2)
     
-    resp.set_cookie('is_mouse','True', max_age=60*60*24*365*2, secure='True', httponly='True')
-    resp.set_cookie('to_mouse','False', max_age=60*60*24*365*2, secure='True', httponly='True')
+    resp.set_cookie('to_mouse','False', max_age=60*60*24*365*2, secure='True')
     
     resp.set_cookie('utag-v2','hhhg1jg312g3j8v7g47v8b3872vb812f3j1kn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th4g4kj25gj235khgh43tkjbvejhg8743t782tiurgkjsfhg98iuwh346t32hj412k', max_age=60*60*24*365*2)
-    resp.set_cookie('uid-vii','ajdhewiuy98hij43bjmv39h983hu843g793bv9kn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th48743g83qh4gy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th438h3', max_age=60*60*24*365*2)
     resp.set_cookie('Gateway_CSRF_Token','8tgh73gh74g4n,i3245b984h974hb78478gh7bq4hg2279h97bh425b98b4h59purh8gh3t8ureg9gvekn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th4rg98hjr4hg98h3g9843g98h34g9839gh9436ghiuhfdkguq3209tukerjgh948ghoegh9opo4u3gih43g93', max_age=60*60*24*365*2)
-    resp.set_cookie('switch-user','True', max_age=60*60*24*365*2)
     resp.set_cookie('btag','hhhg1n5387hb8wtb478bw9hw93ebj89r0ajhw0ma-kwkn34y84g8gb483ut32g3bjy983hfyft32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th443j-e=w-,34h-0wjh708gah8u5jg312g3j12f3j1g4kj25gj235khgh43tkjbvejhg8743t78kn34y84ew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th443j-e=w-,34h-0wjh708gah8u5jg312g3j12f3j1g4kj25gj235khgh43tkjbvejhg8743t78kn34y84g8gb483ut32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th42tiurgkjsfhg98iuwh346t32hj412k', max_age=60*60*24*365*2)
     resp.set_cookie('vid','ajdhewiuy98hij43bjy983hfyfew79gsdlkjfoiwf98732dshf82iv9w84hg9t32g3bjy983hfyfew79gsdlkjfoiwf98732dshf82r9whef98u2fih28fo29ub349th443j-e=w-,34h-0wjh708gah8u5jg312g3j12f3j1g4kj25gj235khgh43tkjbvejhg8743t78kn34y84hq3gw78h3rig9e0shj90heqhg8gqw47rf3y:gubg8sebg84gb78gs4br9whef98u2fih28fo29ub349th438h3', max_age=60*60*24*365*2)
 
@@ -48,9 +45,22 @@ def welcome():
 def cookie_received():
     return render_template("cookie-received.html", title = 'Cookie-received')
 
-@app.route('/ius34fh2siudafh98743', methods=['POST'])
+@app.route('/q2wase4rdzxft6ygvbht4', methods=['POST'])
 def flag():
-    return render_template("flag.html", title = 'Flag')
+
+    if request.method == 'POST':
+        access_token = request.form['acces-token']
+
+        if access_token == "Seriously! You are this Smart. Kudos!!!":
+            return render_template("flag.html", title = 'Flag')
+        else:
+            return redirect("/cookie-received", code = 302)
+    else:
+        return redirect("/", code = 302)
 
 if __name__ == '__main__':
    app.run()
+
+
+# Seriously! You are this Smart. Kudos!!!
+# VlRKV2VXRlhPVEZqTW5nMVNWTkNXbUl6VldkWldFcHNTVWhTYjJGWVRXZFZNakZvWTI1UmRVbEZkREZhUnpsNlNWTkZhQT09
